6cbe89e9a1c0f399eba720ec84dad95a1dc9393b
Nadezhda  Samartseva <nadya.sm101@yandex.com>
2025-11-15 03:14:28 +0300

    lab1: ex7, ex8, ex9 done

--- lab1/7.py
+++ lab1/7.py
@@ -1,33 +1,19 @@
-def network_hidden(inp, weight_i):
-    predict = [0] * len(weight_i)
-    for i in range(len(weight_i)):
-        ws = 0
-        for j in range(len(inp)):
-            ws += inp[j] * weight_i[i][j]
-        predict[i] = ws
-    return 
-
+'''
+Добавьте еще один набор весов (weights_4 = [0.4, 0.2, 0.1]) и добавьте его в список weights. 
+Запустите функцию с этим новым набором весов. 
+Как это повлияло на предсказанные значения? Объясните, почему.
+'''
 def network(inp, weight):
-    predict_hidden = [0] * len(weight[0])
-    for i in range(len(weight[0])):
-        ws = 0
-        for j in range(len(inp)):
-            ws += inp[j] * weight[0][i][j]
-        predict_hidden[i] = ws
-    predict = [0] * len(weight[1])
-    for i in range(len(weight[1])):
-        ws = 0
-        for j in range(len(predict_hidden)):
-            ws += predict_hidden[j] * weight[0][i][j]
-        predict[i] = ws
+    predict = [0] * len(weight)
+    for i in range(len(weight)):
+        predict[i] = sum([inp[j] * weight[i][j] for j in range(len(inp))])
     return predict
 
-inp = [23, 45]
-weight_h_1 = [0.4, 0.1] 
-weight_h_2 = [0.3, 0.2]
-weight_out_1 = [0.4, 0.1] 
-weight_out_2 = [0.3, 0.1] 
-weights_h = [weight_h_1, weight_h_2]
-weights_out = [weight_out_1, weight_out_2]
-weights = [weights_h, weights_out]
-print(network(inp, weights))
+print("1")
+print(network([50, 165], [[0.2, 0.1], [0.3, 0.1]]))
+print("2")
+print(network([50, 165, 45], [[0.2, 0.1, 0.65], [0.3, 0.1, 0.7]]))
+print("3")
+print(network([50, 165, 45], [[0.2, 0.1, 0.65], [0.3, 0.1, 0.7], [0.5, 0.4, 0.34]]))
+print("4")
+print(network([50, 165, 45], [[0.2, 0.1, 0.65], [0.3, 0.1, 0.7], [0.5, 0.4, 0.34], [0.4, 0.2, 0.1]]))
