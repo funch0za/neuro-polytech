f149a2c411beb6dd9850279328edd2220617cc29
Nadezhda  Samartseva <nadya.sm101@yandex.com>
2025-11-18 01:35:30 +0300

    lab2 done

--- lab2/3.py
+++ lab2/3.py
@@ -1,8 +1,6 @@
 '''
-Добавьте еще один скрытый слой к нейросети. 
-Создайте третий набор весов weights_h_3 = [0.6, 0.2] и weights_out_3 = [0.7, 0.4]. 
-Добавьте его в переменные weights_h и weights_out. 
-Измените функцию neural_network так, чтобы она работала с этим третьим слоем.
+Замените списки с фиксированными весами на массивы numpy, 
+причём с генерацией случайных значений в них.
 '''
 import numpy as np
 
@@ -12,15 +10,19 @@ def neuralNetwork(inp, weights):
     prediction_out = prediction_h2.dot(weights[2])
     return prediction_out
 
+
+def get_random_arr(size):
+    return np.random.rand(size)
+
 inp = np.array([23, 45])
 
-weight_h_1 = [0.4, 0.1]
-weight_h_2 = [0.3, 0.2]
-weight_h_3 = [0.6, 0.2]  
+weight_h_1 = get_random_arr(2)
+weight_h_2 = get_random_arr(2)
+weight_h_3 = get_random_arr(2)
 
-weight_out_1 = [0.4, 0.1]
-weight_out_2 = [0.3, 0.1]
-weight_out_3 = [0.7, 0.4]
+weight_out_1 = get_random_arr(2)
+weight_out_2 = get_random_arr(2)
+weight_out_3 = get_random_arr(2)
 
 weights_h = np.array([weight_h_1, weight_h_2, weight_h_3]).T
 weights_out = np.array([weight_out_1, weight_out_2, weight_out_3]).T
