82b68ee0836fdc96bd77a8b4c0be320c0e5af8cc
Nadezhda  Samartseva <nadya.sm101@yandex.com>
2025-11-21 03:29:15 +0300

    lab 4: module 1, module 2

---
+++ lab4/module3/1.py
@@ -0,0 +1,33 @@
+'''
+Напишите по памяти код из урока "Градиентный спуск с несколькими выходами".
+'''
+
+import numpy as np
+
+def neural_networks(inp, weights):
+    return inp * weights
+
+
+def get_error(true_prediction, prediction):
+    return (true_prediction - prediction) ** 2
+
+
+def gradient(inp, weights, true_prediction, count_iters, learning_rate):
+    for i in range(count_iters):
+        prediction = neural_networks(inp, weights)
+        error = get_error(true_prediction, prediction)
+        print("Prediction: %.10f, Weights: %s, Error: %.20f" %(prediction, weights, error))
+        delta = (prediction - true_prediction) * inp * learning_rate
+        delta[0] = 0
+        weights = weights - delta
+
+        
+inp = np.array([150, 40])
+weights = np.array([0.2,0.3])
+true_predictions = np.array([50,120])
+
+count_iters = 30
+
+print("1")
+learning_rate = 0.00001
+count_iters = 1000
